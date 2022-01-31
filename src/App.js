022524206956ee50706d1254e362154198c33003
Jake Toomay <jake.toomay@outlook.com>
2022-01-31 15:02:48 -0600

    Changes I made. Makes the todos get correctly added to the arrays

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import './App.css';
+import "./App.css";
+import Header from "./Components/Header";
+import Todos from "./Components/Todos.js";
+import NewTodoCard from "./Components/NewTodoCard";
+import { useEffect, useState } from "react";
+import { Button, InputGroup, Modal, FormControl } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap
 
 function App() {
+  // use state
+  const [show, setShow] = useState(false);
+
+  // Open Function
+  const handleOpen = () => {
+    setShow(true);
+  };
+
+  // Close Function
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  const [todo, setTodos] = useState([]);
+
+  // * This callback is used to get the data from the todo card and put it back in the app() component
+  // const FunctionName = () => { setFunction( () => [...oldArr, newElement ])}
+  const todoCallback = (task) => {
+    setTodos((todo) => [...todo, task]); // This will add the item that needs to be done to the array that is stored here
+  };
+
   return (
     <div className="App">
-     
+      <h1> Todo List </h1>
+      <Button variant="primary" onClick={() => handleOpen()}>
+        New Todo
+      </Button>
+
+      <Button onClick={() => console.log(todo)}> Show Todo </Button>
+
+      <NewTodoCard
+        open={handleOpen}
+        close={handleClose}
+        show={show}
+        todo={todoCallback}
+      />
     </div>
   );
 }
